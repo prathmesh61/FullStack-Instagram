fda22e7efec63a28db1f2003e8dff6f6716d0f40
prathmesh <prathmeshpol61@gmail.com>
2023-07-23 12:02:55 +0530

    frontend setup done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import HomeScreen from "./screens/HomeScreen";
+
+const App = () => {
+  return (
+    <div className="text-xl font-bold text-center">
+      <HomeScreen />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/main.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+const queryClient = new QueryClient();
+
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
